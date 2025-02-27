c9d986bdc560e05a8f8bd98937f8a5dee4d28338
laylamoguibmm <layla.moguib@student.giu-uni.de>
2025-02-27 23:47:19 +0200

    user schema

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String, // URL of the profile picture
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Standard User", "Organizer", "System Admin"],
+      required: true,
+    },
+  },
+  { timestamps: true } // Automatically adds "createdAt" and "updatedAt"
+);
+
+module.exports = mongoose.model("User", userSchema);
