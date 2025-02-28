80f8d42747e5ddfe8d840c0bc9962b3aa69039bd
gannah <gannah.elbadry@student.giu-uni.de>
2025-02-28 15:13:53 +0200

    gannah's booking part

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,6 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-
-    
+    {
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      event: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Event",
+        required: true,
+      }   
+    }
 )
