ad74c2533e81f814c96fd53bc2e8ba3c0e7cb6a3
My Lap <ganna.rihan@student.giu-uni.de>
2025-02-28 14:34:11 +0200

    Resolved merge conflicts

---
+++ backend/models/Event.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    eventDate: { type: Date, required: true },
+    location: { type: String, required: true },
+    category: { type: String },
+    image: { type: String }, // URL or file path
+    ticketPrice: { type: Number, required: true, min: 0 },
+    totalTickets: { type: Number, required: true, min: 0 },
+    remainingTickets: { type: Number, required: true, min: 0 },
+    organizer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, //reference (ObjectId) to the User model
+    createdAt: { type: Date, default: Date.now } //timestamp? //createdAt automatically stores when the event was created
+});
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = Event;
