0ce49910f71ed383f8430fdec238544b21f0e4da
gannah <gannah.elbadry@student.giu-uni.de>
2025-02-28 15:21:33 +0200

    booking merge conflicts resolved

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-<<<<<<< HEAD
     {
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -12,33 +11,28 @@ const userSchema = new mongoose.Schema(
         type: mongoose.Schema.Types.ObjectId,
         ref: "Event",
         required: true,
-      }   
-    }
-)
-=======
-{
-    numberOfTickets: {
-        type: Number,
-        required: true,
-        min: 1, 
-      },
-      totalPrice: {
-        type: Number, 
-        required: true,
-        min: 0,
-      },
-      status: {
-        type: String, 
-        enum: ["pending", "confirmed", "canceled"],
-        default: "pending", 
-      },
-},
-      {
-        timestamps: true, 
-        }
-
+      }, 
+        numberOfTickets: {
+            type: Number,
+            required: true,
+            min: 1, 
+          },
+          totalPrice: {
+            type: Number, 
+            required: true,
+            min: 0,
+          },
+          status: {
+            type: String, 
+            enum: ["pending", "confirmed", "canceled"],
+            default: "pending", 
+          },
+    },
+          {
+            timestamps: true, 
+            }
     
-);
-
-module.exports = mongoose.model("Booking", bookingSchema);
->>>>>>> c1d5607f92ad011b4ea152833ed4afa57795eaf1
+        
+    );
+    
+    module.exports = mongoose.model("Booking", bookingSchema);
